91af51164d33e6647183335c6ca2a8faa3c979db
kalvin-oanda <kalvinosoro@gmail.com>
2025-09-15 23:29:06 +0300

    sorting the sentence

---
+++ sortingTheSentence/sortingTheSentence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+
+        #iterate through the sentence
+        #separate the sentence into words by after each space
+        #get the last indexed item in each word and take note of the no
+        # rearrange te sentence using the numbers in ascending order
+        i = s.split(' ')
+
+        k = len(i)
+
+        
+
+        print (i)
+       
+        
